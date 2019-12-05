964328ed972472c005fa39dfd489867025b8bb28
Gregory Keeley <gregorykeeley@pursuit.org>
2019-12-04 22:52:30 -0500

    - Created view controllers and segues for ColorDetailsViewController/ RandomUserDetailViewController

---
+++ JSON_Parsing_Lab/JSON_Parsing_Lab/Colors/ColorsDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ColorsDetailViewController.swift
+//  JSON_Parsing_Lab
+//
+//  Created by Gregory Keeley on 12/4/19.
+//  Copyright © 2019 Gregory Keeley. All rights reserved.
+//
+
+import UIKit
+
+class ColorsDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
---
+++ JSON_Parsing_Lab/JSON_Parsing_Lab/RandomUserDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ColorDetailViewController.swift
+//  JSON_Parsing_Lab
+//
+//  Created by Gregory Keeley on 12/4/19.
+//  Copyright © 2019 Gregory Keeley. All rights reserved.
+//
+
+import UIKit
+
+class RandomUserDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}
